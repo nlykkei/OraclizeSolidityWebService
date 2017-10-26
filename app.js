c0089a1b7c069442dd077d17d8571181a05cb272
nlykkei <nlykkei@gmail.com>
2017-10-26 11:09:53 +0200

    POST test.

--- app.js
+++ app.js
@@ -213,6 +213,7 @@ function servePostRequest(req, res) {
 
     req.on('end', function () {
         console.log(buffer.toString('hex'));
+        console.log(buffer.length);
         //console.log(querystring.parse(data));
 
         var nums = [];
